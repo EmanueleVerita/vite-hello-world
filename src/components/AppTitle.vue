e4b014c1270608ce898df6e958d94f85dd70d1c7
EmanueleVerita <emanuele.verita@gmail.com>
2023-07-15 00:08:33 +0200

    esercizio completo

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,39 @@
+<script>
+
+    export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  
+}
+
+</script>
+
+
+<template>
+
+
+  <header>
+
+    <h1>
+      La mia prima App con Vite!!
+    </h1>
+
+  </header>
+
+
+</template>
+
+
+<style scoped>
+
+  header{
+
+    background-color: yellow;
+
+
+  }
+
+</style>
